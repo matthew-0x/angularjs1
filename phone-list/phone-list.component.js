a298ee5bbd63ac91f38045d854a965b56e9f85fb
Matt <mvojdani@gmail.com>
2017-02-01 13:14:21 +1100

    phone-list module/component completed

---
+++ phone-list/phone-list.component.js
@@ -0,0 +1,23 @@
+angular.
+   module('phoneList').
+   component('phoneList',{
+     templateUrl: 'phone-list/phone-list.template.html',
+     controller: function phoneController(){
+       this.phones=[
+         {
+          name: 'Nexus S',
+	  description: 'Very fast'
+         }, {
+
+	  name: 'iPhone 6S',
+          description: 'durable'
+	 
+	 }, {
+          name: 'iPhone 5S',
+          description: 'powerful'
+	 }       
+       ];
+     
+     }
+  });
+   });
